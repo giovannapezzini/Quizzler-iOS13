84719d03c6ea5631858c6ca4706d75e8fe3b4d04
giovannapezzini <giovannarpezzini@gmail.com>
2020-01-20 23:59:58 +0900

    Add questions file

---
+++ Question.swift
@@ -0,0 +1,15 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Giovanna Pezzini on 20/01/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    var text : String
+    var answer : String
+    
+}
